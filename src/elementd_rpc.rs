911186c20f40958e2303637c2b149fa13eccf7ad
Philipp Hoenisch <philipp@hoenisch.at>
2020-11-10 14:53:15 +1100

    Elementd harness using testcontainer

---
+++ src/elementd_rpc.rs
@@ -0,0 +1,75 @@
+use crate::json_rpc;
+use reqwest::Url;
+use serde::{Deserialize};
+use elements::bitcoin::{Network};
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+pub const JSONRPC_VERSION: &str = "1.0";
+
+#[derive(Debug, Clone)]
+pub struct Client {
+    rpc_client: json_rpc::Client,
+}
+
+impl Client {
+    pub fn new(url: Url) -> Self {
+        Client {
+            rpc_client: json_rpc::Client::new(url),
+        }
+    }
+
+    pub async fn network(&self) -> Result<Network> {
+        let blockchain_info = self.blockchain_info().await?;
+
+        Ok(blockchain_info.chain)
+    }
+
+    async fn blockchain_info(&self) -> Result<BlockchainInfo> {
+        let blockchain_info = self
+            .rpc_client
+            .send::<Vec<()>, BlockchainInfo>(json_rpc::Request::new(
+                "getblockchaininfo",
+                vec![],
+                JSONRPC_VERSION.into(),
+            ))
+            .await?;
+
+        Ok(blockchain_info)
+    }
+
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("JSON Rpc: ")]
+    JsonRpc(#[from] json_rpc::Error),
+}
+
+#[derive(Debug, Deserialize)]
+struct BlockchainInfo {
+    chain: Network,
+    mediantime: u32,
+}
+
+#[cfg(all(test))]
+mod test {
+    use super::*;
+    use crate::Elementsd;
+    use testcontainers::clients;
+
+    #[tokio::test]
+    async fn get_network_info() {
+        let tc_client = clients::Cli::default();
+        let (client, _container) = {
+            let blockchain = Elementsd::new(&tc_client, "0.18.1.9").unwrap();
+
+            (Client::new(blockchain.node_url.clone()), blockchain)
+        };
+
+        let network = client.network().await.unwrap();
+
+        assert_eq!(network, Network::Regtest)
+    }
+
+}
---
+++ src/lib.rs
@@ -0,0 +1,67 @@
+#![warn(
+    unused_extern_crates,
+    missing_debug_implementations,
+    missing_copy_implementations,
+    rust_2018_idioms,
+    clippy::cast_possible_truncation,
+    clippy::cast_sign_loss,
+    clippy::fallible_impl_from,
+    clippy::cast_precision_loss,
+    clippy::cast_possible_wrap,
+    clippy::dbg_macro
+)]
+#![forbid(unsafe_code)]
+
+
+pub mod image;
+pub mod elementd_rpc;
+pub mod json_rpc;
+
+use reqwest::Url;
+use testcontainers::{clients, Container, Docker};
+
+pub use crate::elementd_rpc::Client;
+use crate::image::ElementsCore;
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+const ELEMENTSD_RPC_PORT: u16 = 18443;
+
+#[derive(Debug)]
+pub struct Elementsd<'c> {
+    pub container: Container<'c, clients::Cli, ElementsCore>,
+    pub node_url: Url,
+}
+
+impl<'c> Elementsd<'c> {
+    /// Starts a new regtest elementsd container
+    pub fn new(client: &'c clients::Cli, tag: &str) -> Result<Self> {
+        let container = client.run(ElementsCore::default().with_tag(tag));
+        let port = container
+            .get_host_port(ELEMENTSD_RPC_PORT)
+            .ok_or(Error::PortNotExposed(ELEMENTSD_RPC_PORT))?;
+
+        let auth = container.image().auth();
+        let url = format!(
+            "http://{}:{}@localhost:{}",
+            &auth.username, &auth.password, port
+        );
+        let url = Url::parse(&url)?;
+
+        Ok(Self {
+            container,
+            node_url: url,
+        })
+    }
+
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("Elements Rpc: ")]
+    ElementdRpc(#[from] elementd_rpc::Error),
+    #[error("Url Parsing: ")]
+    UrlParseError(#[from] url::ParseError),
+    #[error("Docker port not exposed: ")]
+    PortNotExposed(u16),
+}
