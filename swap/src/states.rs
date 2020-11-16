10d17f6a1df6ccd2fcd5afe388ff083a517ffe0b
Philipp Hoenisch <philipp@hoenisch.at>
2020-11-17 08:57:47 +1100

    WIP: next message from Bob to Alice with first signature

--- swap/src/states.rs
+++ swap/src/states.rs
@@ -1,11 +1,17 @@
 use crate::make_txout;
 use crate::unblind_asset_from_txout;
-use bitcoin::Amount;
 use bitcoin::Script;
+use bitcoin::{Amount, PublicKey};
+use elements_fun::bitcoin::blockdata::opcodes;
+use elements_fun::bitcoin::blockdata::script::Builder;
+use elements_fun::bitcoin::secp256k1::Message;
+use elements_fun::bitcoin::SigHashType;
+use elements_fun::bitcoin_hashes::hash160;
+use elements_fun::bitcoin_hashes::Hash;
 use elements_fun::confidential::Asset;
 use elements_fun::confidential::Nonce;
 use elements_fun::confidential::Value;
-use elements_fun::wally::asset_final_vbf;
+use elements_fun::wally::{asset_final_vbf, tx_get_elements_signature_hash};
 use elements_fun::Address;
 use elements_fun::AssetId;
 use elements_fun::OutPoint;
@@ -39,9 +45,12 @@ pub struct Message1 {
     pub asset_id_commitment_in: Asset,
     pub abf_in: SecretKey,
     pub address_redeem: Address,
+    pub address_change: Address,
     pub abf_redeem: SecretKey,
     pub vbf_redeem: SecretKey,
-    pub signature: Signature,
+    pub abf_change: SecretKey,
+    pub vbf_change: SecretKey,
+    pub witness_stack_bob: Vec<Vec<u8>>,
 }
 
 pub struct Alice0 {
@@ -142,6 +151,7 @@ pub struct Bob0 {
     pub vbf_redeem: SecretKey,
     pub address_change: Address,
     pub abf_change: SecretKey,
+    pub vbf_change: SecretKey,
 }
 
 impl Bob0 {
@@ -176,6 +186,7 @@ impl Bob0 {
         let vbf_redeem = SecretKey::new(rng);
 
         let abf_change = SecretKey::new(rng);
+        let vbf_change = SecretKey::new(rng);
 
         Self {
             amount_alice,
@@ -190,6 +201,7 @@ impl Bob0 {
             vbf_redeem,
             address_change,
             abf_change,
+            vbf_change,
         }
     }
 
@@ -236,7 +248,7 @@ impl Bob0 {
         let change_amount_bob = amount_in_bob - self.amount_bob;
 
         let input_alice = msg.input;
-        let input_bob = self.input;
+        let input_bob = self.input.clone();
 
         let inputs = vec![
             (asset_id_alice, asset_id_commitment_in_alice, abf_in_alice),
@@ -255,7 +267,7 @@ impl Bob0 {
         let redeem_output_bob = make_txout(
             rng,
             self.amount_bob,
-            self.address_redeem,
+            self.address_redeem.clone(),
             self.asset_id_alice,
             *self.abf_redeem.as_ref(),
             *self.vbf_redeem.as_ref(),
@@ -288,7 +300,7 @@ impl Bob0 {
         let change_output_bob = make_txout(
             rng,
             change_amount_bob,
-            self.address_change,
+            self.address_change.clone(),
             asset_id_bob,
             *self.abf_change.as_ref(),
             vbf_change_bob,
@@ -306,7 +318,7 @@ impl Bob0 {
         let transaction = Transaction {
             version: 2,
             lock_time: 0,
-            input: vec![input_alice, input_bob],
+            input: vec![input_alice, input_bob.clone()],
             output: vec![
                 redeem_output_alice.clone(),
                 redeem_output_bob,
@@ -316,11 +328,101 @@ impl Bob0 {
             ],
         };
 
-        Bob1 { transaction }
+        Bob1 {
+            transaction,
+            input_sk: self.input_sk,
+            input_bob,
+            asset_id_bob,
+            asset_id_commitment_in_bob,
+            abf_in_bob,
+            address_redeem_bob: self.address_redeem.clone(),
+            address_change_bob: self.address_change.clone(),
+            abf_redeem: self.abf_redeem,
+            vbf_redeem: self.vbf_redeem,
+            abf_change: self.abf_change,
+            vbf_change: self.vbf_change,
+        }
     }
 }
 
 pub struct Bob1 {
-    // TODO: Add missing info for Message1
     pub transaction: Transaction,
+    pub input_sk: SecretKey,
+    pub input_bob: TxIn,
+    pub asset_id_bob: AssetId,
+    pub asset_id_commitment_in_bob: Asset,
+    pub abf_in_bob: SecretKey,
+    pub address_redeem_bob: Address,
+    pub address_change_bob: Address,
+    pub abf_redeem: SecretKey,
+    pub vbf_redeem: SecretKey,
+    pub abf_change: SecretKey,
+    pub vbf_change: SecretKey,
+}
+
+impl Bob1 {
+    pub fn compose(&self) -> Message1 {
+        let secp = elements_fun::bitcoin::secp256k1::Secp256k1::new();
+
+        let input_pk_bob: PublicKey = todo!();
+        let fund_bitcoin_tx_bob: Transaction = todo!();
+        let fund_bitcoin_tx_vout_bob = todo!();
+        let fund_amount_bob: Value = todo!();
+
+        let witness_stack = {
+            let hash = hash160::Hash::hash(&input_pk_bob.to_bytes());
+            let script = Builder::new()
+                .push_opcode(opcodes::all::OP_DUP)
+                .push_opcode(opcodes::all::OP_HASH160)
+                .push_slice(&hash.into_inner())
+                .push_opcode(opcodes::all::OP_EQUALVERIFY)
+                .push_opcode(opcodes::all::OP_CHECKSIG)
+                .into_script();
+
+            let digest = tx_get_elements_signature_hash(
+                &self.transaction,
+                1, // todo: ensure that this is Bob;s input
+                &script,
+                &fund_amount_bob,
+                1,
+                true,
+            );
+
+            let sig = secp.sign(
+                &Message::from_slice(&digest.into_inner()).unwrap(),
+                &self.input_sk,
+            );
+
+            let mut serialized_signature = sig.serialize_der().to_vec();
+            serialized_signature.push(SigHashType::All as u8);
+
+            vec![serialized_signature, input_pk_bob.to_bytes()]
+        };
+
+        Message1 {
+            input: self.input_bob.clone(),
+            asset_id_in: self.asset_id_bob,
+            asset_id_commitment_in: self.asset_id_commitment_in_bob,
+            abf_in: self.abf_in_bob,
+            address_redeem: self.address_redeem_bob.clone(),
+            address_change: self.address_change_bob.clone(),
+            abf_redeem: self.abf_in_bob,
+            vbf_redeem: self.vbf_redeem,
+            abf_change: self.abf_change,
+            vbf_change: self.vbf_change,
+            witness_stack_bob: witness_stack,
+        }
+    }
+}
+
+pub struct Alice1 {}
+
+impl Alice1 {
+    pub fn interpret(self, msg: Message1) -> Alice1 {
+        // todo verify that what received was expected
+        // extract signature from message and put it into the right spot
+        // sign yourself and put signature in right spot
+        // publish transaction
+        Alice1 {}
+    }
 }
