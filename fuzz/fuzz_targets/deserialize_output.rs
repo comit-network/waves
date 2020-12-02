9ac862e3bae4e5b6db503fbf801954313f77cffa
Lucas Soriano del Pino <l.soriano.del.pino@gmail.com>
2020-12-02 14:26:57 +1100

    Squashed 'elements-fun/' content from commit a40c963
    git-subtree-dir: elements-fun
    git-subtree-split: a40c96372514e01dd87d52aa0b89379dc9d08a69

---
+++ fuzz/fuzz_targets/deserialize_output.rs
@@ -0,0 +1,66 @@
+
+extern crate elements;
+
+fn do_test(data: &[u8]) {
+    let result: Result<elements::TxOut, _> = elements::encode::deserialize(data);
+    match result {
+        Err(_) => {},
+        Ok(output) => {
+            let reser = elements::encode::serialize(&output);
+            assert_eq!(data, &reser[..]);
+
+            output.is_null_data();
+            output.is_pegout();
+            output.pegout_data();
+            output.is_fee();
+            output.minimum_value();
+        },
+    }
+}
+
+#[cfg(feature = "afl")]
+extern crate afl;
+#[cfg(feature = "afl")]
+fn main() {
+    afl::read_stdio_bytes(|data| {
+        do_test(&data);
+    });
+}
+
+#[cfg(feature = "honggfuzz")]
+#[macro_use] extern crate honggfuzz;
+#[cfg(feature = "honggfuzz")]
+fn main() {
+    loop {
+        fuzz!(|data| {
+            do_test(data);
+        });
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    fn extend_vec_from_hex(hex: &str, out: &mut Vec<u8>) {
+        let mut b = 0;
+        for (idx, c) in hex.as_bytes().iter().enumerate() {
+            b <<= 4;
+            match *c {
+                b'A'...b'F' => b |= c - b'A' + 10,
+                b'a'...b'f' => b |= c - b'a' + 10,
+                b'0'...b'9' => b |= c - b'0',
+                _ => panic!("Bad hex"),
+            }
+            if (idx & 1) == 1 {
+                out.push(b);
+                b = 0;
+            }
+        }
+    }
+
+    #[test]
+    fn duplicate_crash() {
+        let mut a = Vec::new();
+        extend_vec_from_hex("00", &mut a);
+        super::do_test(&a);
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,57 @@
+// Rust Elements Library
+// Written in 2018 by
+//   Andrew Poelstra <apoelstra@blockstream.com>
+//
+// To the extent possible under law, the author(s) have dedicated all
+// copyright and related and neighboring rights to this software to
+// the public domain worldwide. This software is distributed without
+// any warranty.
+//
+// You should have received a copy of the CC0 Public Domain Dedication
+// along with this software.
+// If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+//
+
+//! # Rust Elements Library
+//!
+//! Extensions to `rust-bitcoin` to support deserialization and serialization
+//! of Elements transactions and blocks.
+//!
+
+// Coding conventions
+#![deny(non_upper_case_globals)]
+#![deny(non_camel_case_types)]
+#![deny(non_snake_case)]
+#![deny(unused_mut)]
+#![deny(missing_docs)]
+
+pub extern crate bitcoin;
+#[macro_use]
+pub extern crate bitcoin_hashes;
+extern crate slip21;
+#[cfg(feature = "serde")] extern crate serde;
+
+#[cfg(test)] extern crate rand;
+#[cfg(any(test, feature = "serde_json"))] extern crate serde_json;
+
+#[macro_use] mod internal_macros;
+pub mod address;
+pub mod blech32;
+mod block;
+pub mod confidential;
+pub mod dynafed;
+pub mod encode;
+mod fast_merkle_root;
+pub mod issuance;
+mod transaction;
+pub mod slip77;
+
+// export everything at the top level so it can be used as `elements::Transaction` etc.
+pub use address::{Address, AddressParams, AddressError};
+pub use transaction::{OutPoint, PeginData, PegoutData, TxIn, TxOut, TxInWitness, TxOutWitness, Transaction, AssetIssuance};
+pub use block::{BlockHeader, Block};
+pub use block::ExtData as BlockExtData;
+pub use ::bitcoin::consensus::encode::VarInt;
+pub use fast_merkle_root::fast_merkle_root;
+pub use issuance::{AssetId, ContractHash};
+
