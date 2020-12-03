73613b62bf5b07dee26a946f725e9a3136646f9f
Philipp Hoenisch <philipp@hoenisch.at>
2020-12-03 11:57:26 +1100

    Make clippy happy

--- bobtimus/src/main.rs
+++ bobtimus/src/main.rs
@@ -5,8 +5,7 @@ use warp::Filter;
 async fn main() {
     // GET /rate => 200 OK with body returning a random rate
     let routes = warp::path!("rate").map(|| {
-        let rate = "4"; // thrown with a fair dice roll
-        rate
+        "4" // thrown with a fair dice roll
     });
 
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
