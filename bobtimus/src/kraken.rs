d39a517980e5687a0ff101d262e4a28eb6ac7359
Thomas Eizinger <thomas@eizinger.io>
2020-12-17 10:20:15 +1100

    Move rate modules to library
    This allows us to dumb down the binaries and move the tests into the
    appropriate place i.e. next to where the function-under-test is defined.

---
+++ bobtimus/src/kraken.rs
@@ -0,0 +1,197 @@
+use crate::{LatestRate, LiquidUsdt, Rate};
+use anyhow::{anyhow, bail, Result};
+use async_trait::async_trait;
+use futures::{Future, SinkExt, Stream, StreamExt};
+use reqwest::Url;
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use std::convert::TryFrom;
+use tokio::sync::watch;
+use tokio_tungstenite::tungstenite::Message;
+use watch::Receiver;
+
+const KRAKEN_WS_URL: &str = "wss://ws.kraken.com";
+const SUBSCRIBE_XBT_USD_TICKER_PAYLOAD: &str = r#"
+{ "event": "subscribe",
+  "pair": [ "XBT/USD" ],
+  "subscription": {
+    "name": "ticker"
+  }
+}"#;
+
+#[derive(Clone)]
+pub struct RateService {
+    receiver: Receiver<Rate>,
+    latest_rate: Rate,
+}
+
+impl Future for RateService {
+    type Output = Rate;
+
+    fn poll(
+        mut self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Self::Output> {
+        match self.receiver.poll_next_unpin(cx) {
+            std::task::Poll::Ready(Some(rate)) => {
+                self.latest_rate = rate;
+                self.poll(cx)
+            }
+            std::task::Poll::Ready(None) | std::task::Poll::Pending => {
+                std::task::Poll::from(self.latest_rate)
+            }
+        }
+    }
+}
+
+#[async_trait]
+impl LatestRate for RateService {
+    async fn latest_rate(&mut self) -> anyhow::Result<Rate> {
+        Ok(self.await)
+    }
+}
+
+impl RateService {
+    pub async fn new() -> Result<Self> {
+        let (tx, mut rx) = watch::channel(Rate::ZERO);
+
+        let (ws, _response) =
+            tokio_tungstenite::connect_async(Url::parse(KRAKEN_WS_URL).expect("valid url")).await?;
+
+        let (mut write, mut read) = ws.split();
+
+        // TODO: Handle the possibility of losing the connection
+        // to the Kraken WS. Currently the stream would produce no
+        // further items, and consumers would assume that the rate
+        // is up to date
+        tokio::spawn(async move {
+            while let Some(msg) = read.next().await {
+                let msg = match msg {
+                    Ok(Message::Text(msg)) => msg,
+                    _ => continue,
+                };
+
+                let ticker = match serde_json::from_str::<TickerUpdate>(&msg) {
+                    Ok(ticker) => ticker,
+                    _ => continue,
+                };
+
+                let rate = match Rate::try_from(ticker) {
+                    Ok(rate) => rate,
+                    Err(e) => {
+                        log::error!("could not get rate from ticker update: {}", e);
+                        continue;
+                    }
+                };
+
+                let _ = tx.broadcast(rate);
+            }
+        });
+
+        write.send(SUBSCRIBE_XBT_USD_TICKER_PAYLOAD.into()).await?;
+
+        let latest_rate = rx
+            .next()
+            .await
+            .ok_or_else(|| anyhow!("latest rate stream has ended"))?;
+
+        Ok(Self {
+            receiver: rx,
+            latest_rate,
+        })
+    }
+
+    pub fn subscribe(&self) -> impl Stream<Item = Rate> + Clone {
+        self.receiver.clone()
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(transparent)]
+struct TickerUpdate(Vec<TickerField>);
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(untagged)]
+enum TickerField {
+    Data(TickerData),
+    Metadata(Value),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct TickerData {
+    #[serde(rename = "a")]
+    ask: Vec<RateElement>,
+    #[serde(rename = "b")]
+    bid: Vec<RateElement>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(untagged)]
+enum RateElement {
+    Text(String),
+    Number(u64),
+}
+
+impl TryFrom<TickerUpdate> for Rate {
+    type Error = anyhow::Error;
+
+    fn try_from(value: TickerUpdate) -> Result<Self> {
+        let data = value
+            .0
+            .iter()
+            .find_map(|field| match field {
+                TickerField::Data(data) => Some(data),
+                TickerField::Metadata(_) => None,
+            })
+            .ok_or_else(|| anyhow!("ticker update does not contain data"))?;
+
+        let ask = data.ask.first().ok_or_else(|| anyhow!("no ask price"))?;
+        let ask = match ask {
+            RateElement::Text(ask) => LiquidUsdt::from_str_in_dollar(ask)?,
+            _ => bail!("unexpected ask rate element"),
+        };
+
+        let bid = data.bid.first().ok_or_else(|| anyhow!("no bid price"))?;
+        let bid = match bid {
+            RateElement::Text(bid) => LiquidUsdt::from_str_in_dollar(bid)?,
+            _ => bail!("unexpected bid rate element"),
+        };
+
+        Ok(Self { ask, bid })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn deserialize_ticker_update() {
+        let sample_response = r#"
+[2308,{"a":["18215.60000",0,"0.27454523"],"b":["18197.50000",0,"0.63711255"],"c":["18197.50000","0.00413060"],"v":["2.78915585","156.15766485"],"p":["18200.94036","18275.19149"],"t":[22,1561],"l":["18162.40000","17944.90000"],"h":["18220.90000","18482.60000"],"o":["18220.90000","18478.90000"]},"ticker","XBT/USDT"]"#;
+
+        let _ = serde_json::from_str::<TickerUpdate>(sample_response).unwrap();
+    }
+
+    #[tokio::test]
+    async fn latest_rate_does_not_wait_for_next_value() {
+        let (write, read) = watch::channel(Rate::ZERO);
+
+        let latest_rate = Rate {
+            ask: LiquidUsdt::from_str_in_dollar("20000.0").unwrap(),
+            bid: LiquidUsdt::from_str_in_dollar("19000.0").unwrap(),
+        };
+        let _ = write.broadcast(latest_rate).unwrap();
+
+        let mut service = RateService {
+            receiver: read,
+            latest_rate: Rate::ZERO,
+        };
+
+        let rate = service.latest_rate().await.unwrap();
+        assert_eq!(rate, latest_rate);
+
+        let rate = service.latest_rate().await.unwrap();
+        assert_eq!(rate, latest_rate);
+    }
+}
--- bobtimus/src/lib.rs
+++ bobtimus/src/lib.rs
@@ -16,7 +16,9 @@ use swap::states::{Bob0, Message0};
 mod amounts;
 
 pub mod cli;
+pub mod fixed_rate;
 pub mod http;
+pub mod kraken;
 
 pub use amounts::*;
 use elements_fun::bitcoin::secp256k1::{All, Secp256k1};
@@ -190,4 +192,205 @@ impl<R, RS> Bobtimus<R, RS> {
 #[async_trait]
 pub trait LatestRate {
     async fn latest_rate(&mut self) -> Result<Rate>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::fixed_rate;
+    use anyhow::{Context, Result};
+    use elements_fun::{
+        bitcoin::{secp256k1::Secp256k1, Amount, Network, PrivateKey, PublicKey},
+        secp256k1::{rand::thread_rng, SecretKey, SECP256K1},
+        Address, AddressParams, OutPoint, Transaction, TxOut,
+    };
+    use elements_harness::{
+        elementd_rpc::{ElementsRpc, ListUnspentOptions},
+        Client, Elementsd,
+    };
+    use swap::states::{Alice0, Message1};
+    use testcontainers::clients::Cli;
+
+    #[tokio::test]
+    async fn test_handle_swap_request() {
+        let tc_client = Cli::default();
+        let (client, _container) = {
+            let blockchain = Elementsd::new(&tc_client, "0.18.1.9").unwrap();
+
+            (
+                Client::new(blockchain.node_url.clone().into_string()).unwrap(),
+                blockchain,
+            )
+        };
+        let mining_address = client.getnewaddress().await.unwrap();
+
+        let have_asset_id_alice = client.get_bitcoin_asset_id().await.unwrap();
+        let have_asset_id_bob = client.issueasset(100_000.0, 0.0, true).await.unwrap().asset;
+
+        let mut rate_service = fixed_rate::Service::new();
+        let redeem_amount_bob = LiquidBtc::from(Amount::ONE_BTC);
+
+        let rate = rate_service.latest_rate().await.unwrap();
+        let redeem_amount_alice = rate.buy_quote(redeem_amount_bob).unwrap();
+
+        let (
+            fund_address_alice,
+            fund_sk_alice,
+            _fund_pk_alice,
+            fund_blinding_sk_alice,
+            _fund_blinding_pk_alice,
+        ) = make_confidential_address();
+
+        let fund_alice_txid = client
+            .send_asset_to_address(
+                fund_address_alice.clone(),
+                Amount::from(redeem_amount_bob) + Amount::ONE_BTC,
+                Some(have_asset_id_alice),
+            )
+            .await
+            .unwrap();
+        client.generatetoaddress(1, &mining_address).await.unwrap();
+
+        let input_alice = extract_input(
+            &client.get_raw_transaction(fund_alice_txid).await.unwrap(),
+            fund_address_alice,
+        )
+        .unwrap();
+
+        let (
+            final_address_alice,
+            _final_sk_alice,
+            _final_pk_alice,
+            final_blinding_sk_alice,
+            _final_blinding_pk_alice,
+        ) = make_confidential_address();
+
+        let (
+            change_address_alice,
+            _change_sk_alice,
+            _change_pk_alice,
+            change_blinding_sk_alice,
+            _change_blinding_pk_alice,
+        ) = make_confidential_address();
+
+        // move issued asset to wallet address
+        let address = client.getnewaddress().await.unwrap();
+        let _txid = client
+            .send_asset_to_address(
+                address,
+                Amount::from_btc(10.0).unwrap(),
+                Some(have_asset_id_bob),
+            )
+            .await
+            .unwrap();
+        client.generatetoaddress(1, &mining_address).await.unwrap();
+
+        let fee = Amount::from_sat(10_000);
+
+        let alice = Alice0::new(
+            redeem_amount_alice.into(),
+            redeem_amount_bob.into(),
+            input_alice,
+            fund_sk_alice,
+            fund_blinding_sk_alice,
+            have_asset_id_bob,
+            final_address_alice.clone(),
+            final_blinding_sk_alice,
+            change_address_alice.clone(),
+            change_blinding_sk_alice,
+            fee,
+        );
+
+        let message0 = alice.compose();
+
+        let mut bob = Bobtimus {
+            rng: &mut thread_rng(),
+            rate_service,
+            secp: Secp256k1::new(),
+            elementsd: client.clone(),
+            btc_asset_id: have_asset_id_alice,
+            usdt_asset_id: have_asset_id_bob,
+        };
+
+        let transaction = bob
+            .handle_create_swap(CreateSwapPayload {
+                alice_inputs: vec![AliceInput {
+                    outpoint: message0.input.previous_output,
+                    blinding_key: message0.input_blinding_sk,
+                }],
+                address_redeem: message0.address_redeem,
+                address_change: message0.address_change,
+                fee: message0.fee,
+                btc_amount: redeem_amount_bob,
+            })
+            .await
+            .unwrap();
+
+        let transaction = alice.interpret(Message1 { transaction }).unwrap();
+
+        let _txid = client.send_raw_transaction(&transaction).await.unwrap();
+        let _txid = client.generatetoaddress(1, &mining_address).await.unwrap();
+
+        let utxos = client
+            .listunspent(
+                None,
+                None,
+                None,
+                None,
+                Some(ListUnspentOptions {
+                    asset: Some(have_asset_id_alice),
+                    ..Default::default()
+                }),
+            )
+            .await
+            .unwrap();
+
+        let error = 0.0001;
+        assert!(utxos.iter().any(
+            |utxo| (utxo.amount - Amount::from(redeem_amount_bob).as_btc()).abs() < error
+                && utxo.spendable
+        ));
+    }
+
+    fn extract_input(tx: &Transaction, address: Address) -> Result<(OutPoint, TxOut)> {
+        let vout = tx
+            .output
+            .iter()
+            .position(|output| output.script_pubkey() == &address.script_pubkey())
+            .context("Tx doesn't pay to address")?;
+
+        let outpoint = OutPoint {
+            txid: tx.txid(),
+            vout: vout as u32,
+        };
+        let tx_out = tx.output[vout].clone();
+        Ok((outpoint, tx_out))
+    }
+
+    fn make_keypair() -> (SecretKey, PublicKey) {
+        let sk = SecretKey::new(&mut thread_rng());
+        let pk = PublicKey::from_private_key(
+            &SECP256K1,
+            &PrivateKey {
+                compressed: true,
+                network: Network::Regtest,
+                key: sk,
+            },
+        );
+
+        (sk, pk)
+    }
+
+    fn make_confidential_address() -> (Address, SecretKey, PublicKey, SecretKey, PublicKey) {
+        let (sk, pk) = make_keypair();
+        let (blinding_sk, blinding_pk) = make_keypair();
+
+        (
+            Address::p2wpkh(&pk, Some(blinding_pk.key), &AddressParams::ELEMENTS),
+            sk,
+            pk,
+            blinding_sk,
+            blinding_pk,
+        )
+    }
 }
