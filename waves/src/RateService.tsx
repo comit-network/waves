ebc02d0ad8511ce5592cfe8eec20f63ab00c47e2
Philipp Hoenisch <philipp@hoenisch.at>
2020-12-18 13:44:45 +1100

    Add bit/ask helper function

---
+++ waves/src/RateService.tsx
@@ -0,0 +1,34 @@
+import { AssetType, Rate } from "./App";
+
+/**
+ * returns a (beta) amount based on alphaAsset, amount and rate.
+ * Currently only BTC and USDT are accepted
+ * @param alphaAsset needs to be either AssetType.BTC or AssetType.USDT
+ * @param amount the alpha amount is used as the multiplicand
+ * @param rate depicts a bid and ask rate which is used as the multiplier:
+ *  The bid price is what the LP is willing to pay for the currency, while
+ *  the ask price is the rate at which the LP will sell the same currency.
+ */
+export const calculateBetaAmount = (alphaAsset: AssetType, amount: number, rate: Rate) => {
+    type Direction = "ask" | "bid";
+    let direction: Direction = "ask";
+    // we only support these two assets right now
+    switch (alphaAsset) {
+        case AssetType.BTC:
+            direction = "bid";
+            break;
+        case AssetType.USDT:
+            direction = "ask";
+            break;
+        default:
+            // TODO error
+            break;
+    }
+
+    switch (direction) {
+        case "bid":
+            return amount * rate.bid;
+        case "ask":
+            return amount * (1 / rate.ask); // to make it obvious
+    }
+};
