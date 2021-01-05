306026f25dca6b4c63a55e37d7809b8fb5eb6bbc
Thomas Eizinger <thomas@eizinger.io>
2021-01-05 17:08:41 +1100

    Store signatures in actual transaction instead of clone

--- waves/wallet/src/wallet/sign_and_send_swap_transaction.rs
+++ waves/wallet/src/wallet/sign_and_send_swap_transaction.rs
@@ -17,32 +17,38 @@ pub async fn sign_and_send_swap_transaction(
 
     let txouts = get_txouts(&wallet, |utxo, txout| Ok(Some((utxo, txout)))).await?;
 
-    let result = alice_finalize_transaction(transaction, |transaction| async {
+    let result = alice_finalize_transaction(transaction, |mut transaction| async {
         let mut cache = SigHashCache::new(&transaction);
 
-        for (index, input, output) in
-            transaction
-                .clone()
-                .input
-                .iter_mut()
-                .enumerate()
-                .filter_map(|(index, input)| {
-                    txouts
-                        .iter()
-                        .find(|(utxo, _)| {
-                            utxo.txid == input.previous_output.txid
-                                && utxo.vout == input.previous_output.vout
-                        })
-                        .map(|(_, txout)| (index, input, txout))
-                })
-        {
-            input.witness.script_witness = sign_with_key(
-                SECP256K1,
-                &mut cache,
-                index,
-                &wallet.secret_key,
-                output.as_confidential().unwrap().value,
-            );
+        let witnesses = transaction
+            .clone()
+            .input
+            .iter()
+            .enumerate()
+            .filter_map(|(index, input)| {
+                txouts
+                    .iter()
+                    .find(|(utxo, _)| {
+                        utxo.txid == input.previous_output.txid
+                            && utxo.vout == input.previous_output.vout
+                    })
+                    .map(|(_, txout)| (index, txout))
+            })
+            .map(|(index, output)| {
+                let script_witness = sign_with_key(
+                    SECP256K1,
+                    &mut cache,
+                    index,
+                    &wallet.secret_key,
+                    output.as_confidential().unwrap().value,
+                );
+
+                (index, script_witness)
+            })
+            .collect::<Vec<_>>();
+
+        for (index, witness) in witnesses {
+            transaction.input[index].witness.script_witness = witness
         }
 
         Ok(transaction)
