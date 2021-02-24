4156596a34602ad870f00aae7b2b7d94953855ee
Philipp Hoenisch <philipp@hoenisch.at>
2021-02-24 13:54:42 +1100

    Minor refactoring and cleanup of background script.

--- extension/src/background/src/lib.rs
+++ extension/src/background/src/lib.rs
@@ -28,92 +28,96 @@ pub fn main() {
         LOADED_WALLET.lock().await.replace("wallet".to_string());
     });
 
-    let handle_msg_from_cs = Closure::wrap(Box::new(|msg: JsValue, message_sender: JsValue| {
-        if !msg.is_object() {
-            let log = format!("Invalid request: {:?}", msg);
-            log::error!("{}", log);
-            return;
-        }
-
-        let msg: Message = msg.into_serde().unwrap();
-        if msg.target != "background" || msg.source != "content" {
-            log::debug!("Unexpected message: {:?}", msg);
-            return;
-        }
-
-        let message_sender: MessageSender = message_sender.into_serde().unwrap();
-
-        log::info!("BS: Received from CS: {:?}", &msg);
-
-        let popup = Popup {
-            url: format!(
-                "popup.html?content_tab_id={}",
-                message_sender.tab.expect("tab id to exist").id
-            ),
-            type_: "popup".to_string(),
-            height: 200,
-            width: 200,
-        };
-        let js_value = JsValue::from_serde(&popup).unwrap();
-        let object = Object::try_from(&js_value).unwrap();
-        let popup_window = browser.windows().create(&object);
-
-        log::info!("Popup created {:?}", popup_window);
-    }) as Box<dyn Fn(_, _)>);
+    // instantiate listener to receive messages from content script
+    let handle_msg_from_cs = Closure::wrap(Box::new(handle_msg_from_cs) as Box<dyn Fn(_, _)>);
     browser
         .runtime()
         .on_message()
         .add_listener(handle_msg_from_cs.as_ref().unchecked_ref());
     handle_msg_from_cs.forget();
 
-    // receiving transaction to be signed and published
-    let handle_msg_from_ps = Closure::wrap(Box::new(move |js_value: JsValue| {
-        if !js_value.is_object() {
-            let log = format!("Invalid request: {:?}", js_value);
-            log::error!("{}", log);
-            return;
-        }
-
-        let msg: PopupMessage = match js_value.into_serde() {
-            Ok(msg) => msg,
-            Err(_) => {
-                log::debug!("Unexpected message: {:?}", js_value);
-                return;
-            }
-        };
-        if msg.target != "background" || msg.source != "popup" {
-            log::debug!("Unexpected message: {:?}", msg);
-            return;
-        }
-
-        log::info!("Received message from Popup: {:?}", msg);
-
-        spawn_local(async {
-            let guard = LOADED_WALLET.lock().await;
-            let wallet = guard.as_ref().unwrap();
-            log::debug!("Loaded wallet: {}", wallet);
-
-            let _resp = browser.tabs().send_message(
-                msg.content_tab_id,
-                JsValue::from_serde(&Message {
-                    data: msg.data,
-                    target: "content".to_string(),
-                    source: "background".to_string(),
-                })
-                .unwrap(),
-                JsValue::null(),
-            );
-        });
-
-        // TODO: Inform PS about success/failure
-        // Promise::resolve(&JsValue::from_str("ACK2"))
-    }) as Box<dyn FnMut(JsValue)>);
-
+    // instantiate listener to receive messages from popup script
+    let handle_msg_from_ps = Closure::wrap(Box::new(handle_msg_from_ps) as Box<dyn FnMut(JsValue)>);
     browser
         .runtime()
         .on_message()
         .add_listener(handle_msg_from_ps.as_ref().unchecked_ref());
     handle_msg_from_ps.forget();
+}
+
+fn handle_msg_from_ps(js_value: JsValue) {
+    if !js_value.is_object() {
+        let log = format!("BS: Invalid request: {:?}", js_value);
+        log::error!("{}", log);
+        return;
+    }
+
+    let msg: PopupMessage = match js_value.into_serde() {
+        Ok(msg) => msg,
+        Err(_) => {
+            log::debug!("BS: Unexpected message: {:?}", js_value);
+            return;
+        }
+    };
+    if msg.target != "background" || msg.source != "popup" {
+        log::debug!("BS: Unexpected message: {:?}", msg);
+        return;
+    }
+
+    log::info!("BS: Received message from Popup: {:?}", msg);
+
+    spawn_local(async {
+        let guard = LOADED_WALLET.lock().await;
+        let wallet = guard.as_ref().unwrap();
+        log::debug!("BS: Loaded wallet: {}", wallet);
+
+        let _resp = browser.tabs().send_message(
+            msg.content_tab_id,
+            JsValue::from_serde(&Message {
+                data: msg.data,
+                target: "content".to_string(),
+                source: "background".to_string(),
+            })
+            .unwrap(),
+            JsValue::null(),
+        );
+    });
+
+    // TODO: Inform PS about success/failure
+    // Promise::resolve(&JsValue::from_str("ACK2"))
+}
+
+fn handle_msg_from_cs(msg: JsValue, message_sender: JsValue) {
+    if !msg.is_object() {
+        let log = format!("Invalid request: {:?}", msg);
+        log::error!("{}", log);
+        return;
+    }
+
+    let msg: Message = msg.into_serde().unwrap();
+    if msg.target != "background" || msg.source != "content" {
+        log::debug!("BS: Unexpected message: {:?}", msg);
+        return;
+    }
+
+    let message_sender: MessageSender = message_sender.into_serde().unwrap();
+
+    log::info!("BS: Received from CS: {:?}", &msg);
+
+    let popup = Popup {
+        url: format!(
+            "popup.html?content_tab_id={}",
+            message_sender.tab.expect("tab id to exist").id
+        ),
+        type_: "popup".to_string(),
+        height: 200,
+        width: 200,
+    };
+    let js_value = JsValue::from_serde(&popup).unwrap();
+    let object = Object::try_from(&js_value).unwrap();
+    let popup_window = browser.windows().create(&object);
+
+    log::info!("Popup created {:?}", popup_window);
 }
 
 #[derive(Serialize, Deserialize)]
