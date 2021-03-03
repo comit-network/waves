c0ee36e709372a6e8cf9d64a0b090f33e43d1e39
Philipp Hoenisch <philipp@hoenisch.at>
2021-03-03 17:39:28 +1100

    Update e2e test setup

--- e2e_tests/e2e_test_setup.sh
+++ e2e_tests/e2e_test_setup.sh
@@ -11,16 +11,24 @@ response=$(docker exec liquid-e2e-test elements-cli -rpcport=18884 -rpcuser=admi
 usdt_asset_id=$(echo $response | jq -r '.asset')
 echo "USDT Asset ID: "$usdt_asset_id
 
-yarn install
-export NATIVE_ASSET_ID=$native_asset_id
-export USDT_ASSET_ID=$usdt_asset_id
-export CHAIN="ELEMENTS"
-export ESPLORA_API_URL="http://localhost:3012"
-export REACT_APP_BLOCKEXPLORER_URL="http://localhost:5001"
+(
+    cd ../extension
+    yarn install
+    export NATIVE_ASSET_ID=$native_asset_id
+    export USDT_ASSET_ID=$usdt_asset_id
+    export CHAIN="ELEMENTS"
+    export ESPLORA_API_URL="http://localhost:3012"
+    export REACT_APP_BLOCKEXPLORER_URL="http://localhost:5001"
 
-yarn build
-RUST_LOG=info,bobtimus=debug cargo build --bin fake_bobtimus
+    yarn build
+    yarn build:extension
+)
 
-RUST_LOG=info,bobtimus=debug cargo run --bin fake_bobtimus -- \
+(
+    cd ../
+    RUST_LOG=info,bobtimus=debug cargo build --bin fake_bobtimus
+
+    RUST_LOG=info,bobtimus=debug cargo run --bin fake_bobtimus -- \
         --elementsd http://admin1:123@127.0.0.1:7041 \
         --usdt $usdt_asset_id > bobtimus.log 2>&1 &
+)
