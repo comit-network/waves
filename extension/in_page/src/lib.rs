72737c60c5ca5fc202563dd915a6bb195373c495
Philipp Hoenisch <philipp@hoenisch.at>
2021-03-10 14:42:35 +1100

    Cleanup in_page script to reduce redundancy

--- extension/in_page/src/lib.rs
+++ extension/in_page/src/lib.rs
@@ -1,10 +1,8 @@
 use anyhow::Result;
-use elements::Txid;
 extern crate console_error_panic_hook;
-use futures::channel::oneshot;
+use futures::channel::{oneshot, oneshot::Sender};
 use js_sys::{global, Object, Promise};
 use message_types::{ips_cs, ips_cs::RpcData, Component};
-use wallet::CreateSwapPayload;
 use wasm_bindgen::{prelude::*, JsCast};
 use wasm_bindgen_futures::future_to_promise;
 use web_sys::MessageEvent;
@@ -52,169 +50,59 @@ pub fn main() {
 
 #[wasm_bindgen]
 pub fn wallet_status() -> Promise {
-    let (sender, receiver) = oneshot::channel::<JsValue>();
-    let mut sender = Some(sender);
-    // create listener
-    let func = move |msg: MessageEvent| {
-        let js_value: JsValue = msg.data();
-
-        let message: Result<ips_cs::Message, _> = js_value.into_serde();
-        if let Ok(ips_cs::Message {
-            target,
-            rpc_data,
-            source,
-        }) = &message
-        {
-            match (target, source) {
-                (Component::InPage, Component::Content) => {}
-                (_, _) => {
-                    log::warn!("IPS: Unexpected message from: {:?}", message);
-                    return;
-                }
-            }
-
-            log::info!("IPS: Received response from CS: {:?}", rpc_data);
-            if let RpcData::WalletStatus(wallet_status) = rpc_data {
-                sender
-                    .take()
-                    .expect("only send response once")
-                    .send(JsValue::from_serde(wallet_status).unwrap())
-                    .unwrap();
-            }
-        }
-    };
-
-    let cb = Closure::wrap(Box::new(func) as Box<dyn FnMut(MessageEvent)>);
-    let mut listener = Listener::new("message".to_string(), cb);
-
-    let window = web_sys::window().expect("no global `window` exists");
     let js_value = JsValue::from_serde(&ips_cs::Message {
         rpc_data: ips_cs::RpcData::GetWalletStatus,
         target: Component::Content,
         source: Component::InPage,
     })
     .unwrap();
-    window.post_message(&js_value, "*").unwrap();
-
-    let fut = async move {
-        let response = receiver.await;
-        let response = response.map_err(|_| JsValue::from_str("IPS: No response from CS"))?;
-
-        listener.remove();
-        Ok(response)
-    };
-
-    future_to_promise(fut)
+    send_to_cs(&js_value)
 }
 
 #[wasm_bindgen]
 pub fn get_sell_create_swap_payload(btc: String) -> Promise {
-    let (sender, receiver) = oneshot::channel::<CreateSwapPayload>();
-    let mut sender = Some(sender);
-    // create listener
-    let func = move |msg: MessageEvent| {
-        let js_value: JsValue = msg.data();
-
-        let message: Result<ips_cs::Message, _> = js_value.into_serde();
-        if let Ok(ips_cs::Message {
-            target,
-            rpc_data,
-            source,
-        }) = &message
-        {
-            match (target, source) {
-                (Component::InPage, Component::Content) => {}
-                (_, _) => {
-                    log::warn!("IPS: Unexpected message from: {:?}", message);
-                    return;
-                }
-            }
-
-            log::info!("IPS: Received response from CS: {:?}", rpc_data);
-            if let RpcData::SellCreateSwapPayload(payload) = rpc_data.clone() {
-                sender
-                    .take()
-                    .expect("only send response once")
-                    .send(payload)
-                    .unwrap();
-            }
-        }
-    };
-
-    let cb = Closure::wrap(Box::new(func) as Box<dyn FnMut(MessageEvent)>);
-    let mut listener = Listener::new("message".to_string(), cb);
-
-    let window = web_sys::window().expect("no global `window` exists");
     let js_value = JsValue::from_serde(&ips_cs::Message {
         rpc_data: ips_cs::RpcData::GetSellCreateSwapPayload(btc),
         target: Component::Content,
         source: Component::InPage,
     })
     .unwrap();
-    window.post_message(&js_value, "*").unwrap();
-
-    let fut = async move {
-        let response = receiver.await;
-        let response = response.map_err(|_| JsValue::from_str("IPS: No response from CS"))?;
-        let response = JsValue::from_serde(&response).unwrap();
-
-        listener.remove();
-        Ok(response)
-    };
-
-    future_to_promise(fut)
+    send_to_cs(&js_value)
 }
 
 #[wasm_bindgen]
 pub fn get_buy_create_swap_payload(usdt: String) -> Promise {
-    let (sender, receiver) = oneshot::channel::<CreateSwapPayload>();
-    let mut sender = Some(sender);
-    // create listener
-    let func = move |msg: MessageEvent| {
-        let js_value: JsValue = msg.data();
-
-        let message: Result<ips_cs::Message, _> = js_value.into_serde();
-        if let Ok(ips_cs::Message {
-            target,
-            rpc_data,
-            source,
-        }) = &message
-        {
-            match (target, source) {
-                (Component::InPage, Component::Content) => {}
-                (_, _) => {
-                    log::warn!("IPS: Unexpected message from: {:?}", message);
-                    return;
-                }
-            }
-
-            log::info!("IPS: Received response from CS: {:?}", rpc_data);
-            if let RpcData::BuyCreateSwapPayload(payload) = rpc_data.clone() {
-                sender
-                    .take()
-                    .expect("only send response once")
-                    .send(payload)
-                    .unwrap();
-            }
-        }
-    };
-
-    let cb = Closure::wrap(Box::new(func) as Box<dyn FnMut(MessageEvent)>);
-    let mut listener = Listener::new("message".to_string(), cb);
-
-    let window = web_sys::window().expect("no global `window` exists");
     let js_value = JsValue::from_serde(&ips_cs::Message {
         rpc_data: ips_cs::RpcData::GetBuyCreateSwapPayload(usdt),
         target: Component::Content,
         source: Component::InPage,
     })
     .unwrap();
-    window.post_message(&js_value, "*").unwrap();
+    send_to_cs(&js_value)
+}
+
+#[wasm_bindgen]
+pub fn sign_and_send(tx_hex: String) -> Promise {
+    let js_value = JsValue::from_serde(&ips_cs::Message {
+        rpc_data: ips_cs::RpcData::SignAndSend(tx_hex),
+        target: Component::Content,
+        source: Component::InPage,
+    })
+    .unwrap();
+    send_to_cs(&js_value)
+}
+
+fn send_to_cs(js_value: &JsValue) -> Promise {
+    let (sender, receiver) = oneshot::channel::<JsValue>();
+    let sender = Some(sender);
+    let mut listener = create_listener(sender);
+
+    let window = web_sys::window().expect("no global `window` exists");
+    window.post_message(js_value, "*").unwrap();
 
     let fut = async move {
         let response = receiver.await;
         let response = response.map_err(|_| JsValue::from_str("IPS: No response from CS"))?;
-        let response = JsValue::from_serde(&response).unwrap();
 
         listener.remove();
         Ok(response)
@@ -223,10 +111,7 @@ pub fn get_buy_create_swap_payload(usdt: String) -> Promise {
     future_to_promise(fut)
 }
 
-#[wasm_bindgen]
-pub fn sign_and_send(tx_hex: String) -> Promise {
-    let (sender, receiver) = oneshot::channel::<Txid>();
-    let mut sender = Some(sender);
+fn create_listener(mut sender: Option<Sender<JsValue>>) -> Listener<dyn FnMut(MessageEvent)> {
     // create listener
     let func = move |msg: MessageEvent| {
         let js_value: JsValue = msg.data();
@@ -247,38 +132,49 @@ pub fn sign_and_send(tx_hex: String) -> Promise {
             }
 
             log::info!("IPS: Received response from CS: {:?}", rpc_data);
-            if let RpcData::SwapTxid(txid) = rpc_data {
-                sender
-                    .take()
-                    .expect("only send response once")
-                    .send(*txid)
-                    .unwrap();
+            match rpc_data {
+                RpcData::WalletStatus(wallet_status) => {
+                    sender
+                        .take()
+                        .expect("only send response once")
+                        .send(JsValue::from_serde(wallet_status).unwrap())
+                        .unwrap();
+                }
+                RpcData::SellCreateSwapPayload(payload) => {
+                    sender
+                        .take()
+                        .expect("only send response once")
+                        .send(JsValue::from_serde(&payload).unwrap())
+                        .unwrap();
+                }
+                RpcData::BuyCreateSwapPayload(payload) => {
+                    sender
+                        .take()
+                        .expect("only send response once")
+                        .send(JsValue::from_serde(&payload).unwrap())
+                        .unwrap();
+                }
+                RpcData::SwapTxid(txid) => {
+                    sender
+                        .take()
+                        .expect("only send response once")
+                        .send(JsValue::from_serde(txid).unwrap())
+                        .unwrap();
+                }
+                rpc_data => {
+                    let msg = format!("Received unsupported message from CS: {:?}", rpc_data);
+                    sender
+                        .take()
+                        .expect("only send response once")
+                        .send(JsValue::from_str(&msg))
+                        .unwrap();
+                }
             }
         }
     };
 
     let cb = Closure::wrap(Box::new(func) as Box<dyn FnMut(MessageEvent)>);
-    let mut listener = Listener::new("message".to_string(), cb);
-
-    let window = web_sys::window().expect("no global `window` exists");
-    let js_value = JsValue::from_serde(&ips_cs::Message {
-        rpc_data: ips_cs::RpcData::SignAndSend(tx_hex),
-        target: Component::Content,
-        source: Component::InPage,
-    })
-    .unwrap();
-    window.post_message(&js_value, "*").unwrap();
-
-    let fut = async move {
-        let response = receiver.await;
-        let response = response.map_err(|_| JsValue::from_str("IPS: No response from CS"))?;
-        let response = JsValue::from_serde(&response).unwrap();
-
-        listener.remove();
-        Ok(response)
-    };
-
-    future_to_promise(fut)
+    Listener::new("message".to_string(), cb)
 }
 
 struct Listener<F>
