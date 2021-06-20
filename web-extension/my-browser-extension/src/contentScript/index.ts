de452275b9b96d353d1720a60bf70519964caf74
Philipp Hoenisch <philipp@hoenisch.at>
2021-06-20 15:52:17 +1000

    Load wasm lib in background script.

--- web-extension/my-browser-extension/src/contentScript/index.ts
+++ web-extension/my-browser-extension/src/contentScript/index.ts
@@ -1,3 +1,8 @@
+import Debug from "debug";
+
+Debug.enable("*");
+const debug = Debug("content");
+
 // If your extension doesn't need a content script, just leave this file empty
 
 // This is an example of a script that will run on every page. This can alter pages
@@ -10,10 +15,10 @@ export function printAllPageLinks() {
         link => link.href,
     );
 
-    console.log("-".repeat(30));
-    console.log(
+    debug("-".repeat(30));
+    debug(
         `These are all ${allLinks.length} links on the current page that have been printed by the Sample Create React Extension`,
     );
-    console.log(allLinks);
-    console.log("-".repeat(30));
+    debug(allLinks);
+    debug("-".repeat(30));
 }
---
+++ web-extension/my-browser-extension/wallet/src/lib.rs
@@ -0,0 +1,13 @@
+use log::Level;
+
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn setup() {
+    #[cfg(feature = "console_error_panic_hook")]
+    console_error_panic_hook::set_once();
+
+    wasm_logger::init(wasm_logger::Config::new(Level::Debug));
+
+    log::info!("Hello World from WASM");
+}
