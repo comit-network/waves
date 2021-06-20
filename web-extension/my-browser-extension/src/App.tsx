88812dacfe8e8c63f4054082eca6e7940e94ab18
Philipp Hoenisch <philipp@hoenisch.at>
2021-06-20 11:42:34 +1000

    Init commit for a react-based browser extension.

---
+++ web-extension/my-browser-extension/src/App.tsx
@@ -0,0 +1,29 @@
+import { Box, ChakraProvider, Code, Grid, Link, Text, theme, VStack } from "@chakra-ui/react";
+import * as React from "react";
+import { Logo } from "./Logo";
+
+const App = () => (
+    <ChakraProvider theme={theme}>
+        <Box textAlign="center" fontSize="xl">
+            <Grid minH="100vh" p={3}>
+                <VStack spacing={8}>
+                    <Logo h="40vmin" pointerEvents="none" />
+                    <Text>
+                        Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
+                    </Text>
+                    <Link
+                        color="teal.500"
+                        href="https://chakra-ui.com"
+                        fontSize="2xl"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        COMIT Waves
+                    </Link>
+                </VStack>
+            </Grid>
+        </Box>
+    </ChakraProvider>
+);
+
+export default App;
---
+++ web-extension/my-browser-extension/src/options/Options.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import Options from "./Options";
+
+test("renders learn react link", () => {
+    render(<Options />);
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+});
