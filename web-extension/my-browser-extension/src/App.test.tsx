7113c21b3f3ad400320a5d4b76a43e77a5d26f65
Philipp Hoenisch <philipp@hoenisch.at>
2021-06-20 11:42:34 +1000

    Init commit for a react-based browser extension.

---
+++ web-extension/my-browser-extension/src/App.test.tsx
@@ -0,0 +1,10 @@
+import { screen } from "@testing-library/react";
+import React from "react";
+import App from "./App";
+import { render } from "./test-utils";
+
+test("renders learn react link", () => {
+    render(<App />);
+    const linkElement = screen.getByText(/COMIT Waves/i);
+    expect(linkElement).toBeInTheDocument();
+});
---
+++ web-extension/my-browser-extension/src/background/index.ts
@@ -0,0 +1,12 @@
+import Debug from "debug";
+
+Debug.enable("*");
+const debug = Debug("background");
+
+messageInBackground();
+
+// This needs to be an export due to typescript implementation limitation of needing '--isolatedModules' tsconfig
+export function messageInBackground() {
+    debug("I can run your javascript like any other code in your project");
+    debug("just do not forget, I cannot render anything !");
+}
