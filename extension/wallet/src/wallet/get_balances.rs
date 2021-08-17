1c9f0e1a8b1b69e0678c2a5918b9863624cc35f5
Thomas Eizinger <thomas@eizinger.io>
2021-08-17 17:14:55 +1000

    Return balances as expected by frontend

--- extension/wallet/src/wallet/get_balances.rs
+++ extension/wallet/src/wallet/get_balances.rs
@@ -1,8 +1,10 @@
 use crate::{
+    assets,
     esplora::EsploraClient,
-    wallet::{current, BalanceEntry, Wallet},
+    wallet::{current, Wallet},
 };
 use anyhow::Result;
+use elements::AssetId;
 use futures::lock::Mutex;
 
 pub async fn get_balances(
@@ -13,7 +15,23 @@ pub async fn get_balances(
     let mut wallet = current(name, current_wallet).await?;
     wallet.sync(client).await?;
 
-    let balances = wallet.compute_balances();
+    let balances = wallet
+        .compute_balances()
+        .into_iter()
+        .map(|e| BalanceEntry {
+            asset: e.asset,
+            value: e.value,
+            ticker: assets::lookup(e.asset).map(|(ticker, _)| ticker.to_owned()),
+        })
+        .collect();
 
     Ok(balances)
+}
+
+/// A single balance entry as returned by [`get_balances`].
+#[derive(Clone, Debug, serde::Serialize, serde::Deserialize, PartialEq)]
+pub struct BalanceEntry {
+    pub asset: AssetId,
+    pub value: u64,
+    pub ticker: Option<String>,
 }
